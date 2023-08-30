40dc596be0f05e3103187c5e84973c9a459f4d71
Ansh <anshinbox44@gmail.com>
2023-08-30 18:48:03 +0530

    Data_Mining

---
+++ Data_Mining/database_orm.py
@@ -0,0 +1,31 @@
+#sqlalchemy
+
+from sqlalchemy.ext import declarative
+from sqlalchemy import Column, String, Boolean, Integer
+from sqlalchemy import engine
+
+Base = declarative.declarative_base()
+
+class Product(Base):
+    __tablename__ = 'Products'
+    id = Column(Integer, primary_key=True)
+    link = Column(String)
+    is_sponsored = Column(Boolean, default=False)
+    title = Column(String)
+    brand = Column(String)
+    price = Column(String)
+    oprice= Column(String)
+    discount = Column(String)
+    delivery = Column(String)
+    deal_type = Column(String, default="")
+    color = Column(String)
+    is_sponsored = Column(Boolean, default=False)
+    is_assured = Column(Boolean, default=False)
+    
+    def __str__(self):
+        return self.title
+    
+if __name__ == "__main__":
+    engine = engine.create_engine('sqlite:///scraper.sqlite',
+                                 echo=True)
+    Base.metadata.create_all(engine)
